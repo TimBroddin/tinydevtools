331f8d650c45b5610eee181c845d0aeb5e4469ae
Tim Broddin <tim@broddin.be>
2025-05-29 08:50:30 +0200

    Update title format for Tailwind Colors page

--- src/routes/tailwind/colors.tsx
+++ src/routes/tailwind/colors.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute('/tailwind/colors')({
     return {
       meta: [
         {
-          title: "tinydev.tools - Tailwind Colors",
+          title: "Tailwind Colors - tinydev.tools",
         },
       ],
     };
